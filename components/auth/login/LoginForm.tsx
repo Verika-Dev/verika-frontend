7e13cc67624e11feca61aecf5ef70af8e8b03c1d
9ightshade <aniekemeusanga@gmail.com>
2025-11-07 18:32:40 +0100

    remove shield

--- components/auth/login/LoginForm.tsx
+++ components/auth/login/LoginForm.tsx
@@ -49,7 +49,7 @@ function LoginForm() {
           </label>
           <div className="relative">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-              <Shield className="h-5 w-5 text-gray-400" />
+              {/* <Shield className="h-5 w-5 text-gray-400" /> */}
             </div>
             <select
               id="role"
